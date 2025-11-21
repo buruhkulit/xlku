26268c55e794128b968983b1974054d87c8690ab
Buruh Kulit <buruhkulit@gmail.com>
2025-11-21 17:28:47 +0700

    Update util.py

--- app/menus/util.py
+++ app/menus/util.py
@@ -6,7 +6,11 @@ import textwrap
 def clear_screen():
     print("Clearing screen...")
     os.system('cls' if os.name == 'nt' else 'clear')
-    print("𝔹𝕦𝕣𝕦𝕙 𝕂𝕦𝕝𝕚𝕥".center(WIDTH))
+    ascii_art = r"""
+𝔹𝕦𝕣𝕦𝕙 𝕂𝕦𝕝𝕚𝕥
+"""
+
+    print(ascii_art)
 
 def pause():
     input("\nPress enter to continue...")
